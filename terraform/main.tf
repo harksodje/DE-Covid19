aacd29362c2f017bb5fa7044101162d1b2bba92f
root <root@DESKTOP-KKDQBBO>
2024-04-01 23:34:57 +0900

    gke-storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,16 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
 
+module "bucket" {
+  source  = "./modules/bucket"
+  project = var.project
+  region  = var.region
+  
+}
+
+module "gke" {
+  source  = "./modules/gke"
+  service_account_email = var.service_account_email
+  prefix  = var.prefix
+  
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,4 @@ variable "prefix" {}
 variable "region" {}
 variable "project" {}
 variable "bucket-name" {}
+variable "service_account_email" {}
