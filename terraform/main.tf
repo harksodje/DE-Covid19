ccece17cb8f30381e0b17f3efcee269ffe28582d
root <root@DESKTOP-KKDQBBO>
2024-04-02 16:03:02 +0900

    restart

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
-# data "google_client_config" "default" {}
+data "google_client_config" "default" {}
 
-# provider "kubernetes" {
-#   host                   = "https://${module.gke.endpoint}"
-#   token                  = data.google_client_config.default.access_token
-#   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-# }
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
 
-# module "bucket" {
-#   source  = "./modules/storage"
-#   bucket-name = var.bucket-name
-#     location = var.region
-#     prefix   = var.prefix
+module "bucket" {
+  source  = "./modules/storage"
+  bucket-name = var.bucket-name
+    location = var.region
+    prefix   = var.prefix
   
-# }
+}
 
-# module "gke" {
-#   source  = "./modules/gke"
-#   service_account_email = var.service_account_email
-#   prefix  = var.prefix
+module "gke" {
+  source  = "./modules/gke"
+  service_account_email = var.service_account_email
+  prefix  = var.prefix
   
-# }
+}
